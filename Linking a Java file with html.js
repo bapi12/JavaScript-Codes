513bb084ee22eef3226a149b817336441cfc688e
bapi12 <86040135+bapi12@users.noreply.github.com>
2021-06-17 11:41:15 +0530

    Add files via upload

---
+++ Linking a Java file with html.js
@@ -0,0 +1,13 @@
+//Internal javascript
+      console.log('Hellow" World');
+      console.log("I am Subhajit");
+
+      window.alert("Aisha");
+      document.write(90+10);
+      document.write("90+10");
+      document.write("<h1>This is Our Javascript Program</h1>");
+
+      var a="Aisha is a student of BCA";
+      var c="She is a student of 1st YEAR";
+    //   var b=9;
+      console.log(`${a} She ""has done '' her '' 1st sem ${c}`);//`${}`
